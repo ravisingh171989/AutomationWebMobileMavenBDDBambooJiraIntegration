5b545c30318d7cebfe3344ac16ec74136af10d3a
AnandRavi <ravisingh17989@gmail.com>
2023-08-27 23:57:03 +1000

    First commit for this framework

---
+++ src/main/java/Hooks/Hooks.java
@@ -0,0 +1,10 @@
+package Hooks;
+
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Hooks {
+    private WebDriver driver;
+}
