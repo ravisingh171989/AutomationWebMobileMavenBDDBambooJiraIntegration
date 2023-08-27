e3485741745727989522c355250502ad60700ced
pallavi singh <pspallavi024@gmail.com>
2023-08-27 23:26:17 +1000

    additional skeleton for framework

---
+++ src/main/java/Constants/FrameworkConstants.java
@@ -0,0 +1,18 @@
+/*
+ *   Author: Anand Ravi
+ *   https://www.linkedin.com/in/anandravisingh/
+ *   https://github.com/ravisingh171989
+ *   https://anand.github.io/
+ *
+ * */
+
+package Constants;
+
+public class FrameworkConstants {
+	public static final int EXPLICIT_WAIT = 10;
+	public static final String PROJECT_NAME = "Automation Web and Mobile Maven BDD with Bamboo Jira Integration";
+
+	public static final String PARAMETER_ENV = "env";
+	public static final String PARAMETER_BROWSER = "browser";
+	public static final String BROWSER_CHROME = "chrome";
+}
