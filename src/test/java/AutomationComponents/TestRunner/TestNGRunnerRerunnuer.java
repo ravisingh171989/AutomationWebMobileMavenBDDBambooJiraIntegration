e1c9a9aca76f575eb3441255a840875c7a3da94d
ravisingh171989 <ravisingh171989@gmail.com>
2024-05-02 15:03:45 +1000

    Create TestNGRunnerRerunnuer.java

---
+++ src/test/java/AutomationComponents/TestRunner/TestNGRunnerRerunnuer.java
@@ -0,0 +1,23 @@
+package AutomationComponents.TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        tags = "",
+        features = "@target/TestFailedScenarios/failedrerun.txt",
+        glue = { "AutomationComponents.Hooks","AutomationComponents/StepDefinition" },
+        plugin = {
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "pretty",
+                "json:target/cucumber-reports/cucumber.json",
+                "html:target/cucumber-reports/cucumber.html"}
+)
+public class TestNGRunnerRerunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
