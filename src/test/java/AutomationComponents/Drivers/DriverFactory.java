af4d28fd0be91dacb7b66275013faa0deb87cf5c
ravisingh171989 <ravisingh171989@gmail.com>
2024-05-02 14:54:33 +1000

    Update DriverFactory.java

--- src/test/java/AutomationComponents/Drivers/DriverFactory.java
+++ src/test/java/AutomationComponents/Drivers/DriverFactory.java
@@ -8,25 +8,37 @@
 
 package AutomationComponents.Drivers;
 
+import AutomationComponents.Worlds.World;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.http.ClientConfig;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.Duration;
 
 public class DriverFactory {
+	World world;
+	WebDriver driver;
 
-	public static WebDriver initializeDriver(String browser) throws MalformedURLException, URISyntaxException {
-		WebDriver driver = null;
-		browser = System.getProperty("browser");
-		String executionType = System.getProperty("executionType");
-		String url = "http://localhost:4444/wd/hub";
+	public DriverFactory(World world) {
+		this.world = world;
+	}
+
+	public WebDriver initializeDriver(String browser, String executionType) throws MalformedURLException, URISyntaxException {
+		DevTools devTools;
+		String url = "http://192.168.1.108:4444/wd/hub";
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setBrowserName(browser);
 		desiredCapabilities.setPlatform(Platform.ANY);
@@ -34,7 +46,14 @@ public class DriverFactory {
 		if (browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			if (executionType.equalsIgnoreCase("remote")) {
-				driver = new RemoteWebDriver(uri.toURL(), desiredCapabilities);
+				ClientConfig config = ClientConfig.defaultConfig().connectionTimeout(Duration.ofMinutes(20))
+						.readTimeout(Duration.ofMinutes(20)); // I change this 3 minute(Default) to 20 minutes.
+				ChromeOptions options = new ChromeOptions();
+				driver = RemoteWebDriver.builder().oneOf(desiredCapabilities).address(uri).config(config).build(); // now you can use this remoteWebDriver.
+				//driver = new RemoteWebDriver(new URL(url), options);
+				driver = new Augmenter().augment(driver);
+				devTools = ((HasDevTools) driver).getDevTools();
+				devTools.createSession();
 			} else if (executionType.equalsIgnoreCase("local")) {
 				driver = new ChromeDriver();
 			}
